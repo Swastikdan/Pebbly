5bb29e1fbe1694ea2ec5649b77207630a0cc0060
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2026-06-01 22:43:25 +0530

    feat: update site metadata with a comprehensive set of favicon and touch icon assets

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -45,6 +45,30 @@ export const Route = createRootRouteWithContext<RouterContext>()({
 				content: SITE_CONFIG.name,
 			},
 			{
+				name: "msapplication-TileColor",
+				content: "#0b0a08",
+			},
+			{
+				name: "msapplication-TileImage",
+				content: "/mstile-144x144.png",
+			},
+			{
+				name: "msapplication-square70x70logo",
+				content: "/mstile-70x70.png",
+			},
+			{
+				name: "msapplication-square150x150logo",
+				content: "/mstile-150x150.png",
+			},
+			{
+				name: "msapplication-wide310x150logo",
+				content: "/mstile-310x150.png",
+			},
+			{
+				name: "msapplication-square310x310logo",
+				content: "/mstile-310x310.png",
+			},
+			{
 				name: "keywords",
 				content:
 					"movie database, film reviews, TV show information, movie ratings, entertainment",
@@ -124,6 +148,76 @@ export const Route = createRootRouteWithContext<RouterContext>()({
 				href: "https://ik.imagekit.io/swastikdan/Film-Fanatic/public/favicon-16x16.png",
 				sizes: "16x16",
 				type: "image/png",
+			},
+			{
+				rel: "apple-touch-icon-precomposed",
+				sizes: "57x57",
+				href: "/apple-touch-icon-57x57.png",
+			},
+			{
+				rel: "apple-touch-icon-precomposed",
+				sizes: "114x114",
+				href: "/apple-touch-icon-114x114.png",
+			},
+			{
+				rel: "apple-touch-icon-precomposed",
+				sizes: "72x72",
+				href: "/apple-touch-icon-72x72.png",
+			},
+			{
+				rel: "apple-touch-icon-precomposed",
+				sizes: "144x144",
+				href: "/apple-touch-icon-144x144.png",
+			},
+			{
+				rel: "apple-touch-icon-precomposed",
+				sizes: "60x60",
+				href: "/apple-touch-icon-60x60.png",
+			},
+			{
+				rel: "apple-touch-icon-precomposed",
+				sizes: "120x120",
+				href: "/apple-touch-icon-120x120.png",
+			},
+			{
+				rel: "apple-touch-icon-precomposed",
+				sizes: "76x76",
+				href: "/apple-touch-icon-76x76.png",
+			},
+			{
+				rel: "apple-touch-icon-precomposed",
+				sizes: "152x152",
+				href: "/apple-touch-icon-152x152.png",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				href: "/favicon-196x196.png",
+				sizes: "196x196",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				href: "/favicon-96x96.png",
+				sizes: "96x96",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				href: "/favicon-32x32.png",
+				sizes: "32x32",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				href: "/favicon-16x16.png",
+				sizes: "16x16",
+			},
+			{
+				rel: "icon",
+				type: "image/png",
+				href: "/favicon-128.png",
+				sizes: "128x128",
 			},
 			{
 				rel: "apple-touch-startup-image",
